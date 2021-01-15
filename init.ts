277b328d1ad008b8e8dc909b6640c3c688991021
Shush <almogosheriko@gmail.com>
2021-01-15 21:07:31 +0200

    Update init.ts

--- init.ts
+++ init.ts
@@ -4,27 +4,45 @@ function copyFiles() {
 	console.log("Copying initial files...");
 	const resourcePath = "node_modules/~resource";
 	let filePath = __dirname + "/tsconfig.json";
-	if (fs.existsSync(filePath) && fs.existsSync(resourcePath)) {
-		let resultPath = resourcePath + "/tsconfig.json";
-		fs.copyFileSync(filePath, resultPath);		
-	}
-
-	filePath = __dirname + "/ModifierProperties.d.ts";
-	if (fs.existsSync(filePath) && fs.existsSync(resourcePath))
+	if (fs.existsSync(resourcePath)) 
 	{
-		let resultPath = resourcePath + "/ModifierProperties.d.ts"		
-		fs.copyFileSync(filePath, resultPath);
+		console.log(`Checking if tsconfig.json exists in ${filePath}`)
+		if (fs.existsSync(filePath))
+		{
+			console.log("tsconfig.json found!")
+			let resultPath = resourcePath + "/tsconfig.json";
+			fs.copyFileSync(filePath, resultPath);		
+			console.log(`tsconfig.json copied to ${resultPath} folder`);
+		}
+		filePath = __dirname + "node_modules/@shushishtok/tooltip_generator/ModifierProperties.d.ts";
+		console.log(`Checking if ModifierProperties.d.ts exists in ${filePath}`)
+		if (fs.existsSync(filePath))
+		{
+			console.log("ModifierProperties.d.ts found!")
+			let resultPath = resourcePath + "/ModifierProperties.d.ts"		
+			fs.copyFileSync(filePath, resultPath);
+			console.log(`ModifierProperties.d.ts copied to ${resultPath} folder`);
+		}
 	}
+	
 	let dirPath = __dirname + "/localization";
-	if (fs.existsSync(filePath) && fs.existsSync(resourcePath)) {
+	if (fs.existsSync(filePath) && fs.existsSync(resourcePath)) 
+	{
 		let resultPath = resourcePath + "/localization";
-		if (!fs.existsSync(resultPath)) {
+		console.log(`Checking if localization folder exists in ${resultPath}`);
+		if (!fs.existsSync(resultPath)) 
+		{
 			fs.mkdirSync(resultPath);
+			console.log("localization folder not found, creating the folder.")
 		}
-		if (!fs.existsSync(resultPath + "/localizationData.ts")) {
+		console.log(`Checking if localizationData example files exist in folder`);
+		if (!fs.existsSync(resultPath + "/localizationData.ts")) 
+		{
 			fs.copyFileSync(dirPath + "/localizationData.ts", resultPath + "/localizationData.ts");
+			console.log(`localizationData example files do not exist: created at ${resultPath}`)
 		}
-		if (!fs.existsSync(resultPath + "/localizationData.js")) {
+		if (!fs.existsSync(resultPath + "/localizationData.js")) 
+		{
 			fs.copyFileSync(dirPath + "/localizationData.js", resultPath + "/localizationData.js");
 		}
 	}
